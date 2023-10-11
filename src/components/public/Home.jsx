d67eefead1125dc61509e524e4064e45d4808b0a
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-11 23:10:27 +0530

    fixed the initial bugs

---
+++ src/components/public/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import NavigationBar from "./NavigationBar";
+
+export default function Home(props) {
+  return (
+    <>
+      <NavigationBar />
+      <h1>Home</h1>
+    </>
+  );
+}
