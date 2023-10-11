14b544185d575618fea2cd7c02569add8c38bba8
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-11 23:38:40 +0530

    added the navigation bar links

--- src/components/public/NavigationBar.jsx
+++ src/components/public/NavigationBar.jsx
@@ -3,52 +3,54 @@ import { React } from "react";
 export default function NavigationBar(props) {
   return (
     <>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg bg-light">
-          <div className="container-fluid">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarTogglerDemo01"
-              aria-controls="navbarTogglerDemo01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <a className="navbar-brand" href="/">
-                Hidden brand
-              </a>
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="/">
+              PowerInformer
+            </a>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="/home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/signin">
+                  Sign In
+                </a>
+              </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="/contact">
-                    Contact
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/signin">
-                    Sign In
-                  </a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link" href="/singup">
-                    Sign Up
-                  </a>
-                </li>
-              </ul>
-            </div>
+              <li className="nav-item">
+                <a className="nav-link" href="/singup">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </>
   );
 }
